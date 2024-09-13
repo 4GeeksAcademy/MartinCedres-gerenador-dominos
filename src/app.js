62473862ce471911702021c7b5764a97f6a9c580
Martin-cedres <martinfernandocedres@gmail.com>
2024-09-13 23:43:32 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("generador de dominios");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "raacon"];
+  let ext = [".com", ".uy", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+        }
+      }
+    }
+  }
 };
